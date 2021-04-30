2bb8688c3d76e0369f6fe9e8d48df5539b1397d8
rbalma <balmarodrigo45@gmail.com>
2021-04-30 15:53:26 -0300

    Protección de rutas

--- routes/operation.js
+++ routes/operation.js
@@ -8,11 +8,11 @@ const operationsController = require('../controllers/operationsController');
 
 
 router.get('/operations/:userId', auth, operationsController.getOperations);
-router.post('/operations', operationsController.newOperations);
-router.put('/operations/update/:id', operationsController.updateOperations);
-router.delete('/operations/delete/:id', operationsController.deleteOperations);
-router.get('/operations/get/:id', operationsController.getOperationById);
-router.get('/operations/prices/:userId', operationsController.getPrices);
+router.post('/operations', auth, operationsController.newOperations);
+router.put('/operations/update/:id', auth, operationsController.updateOperations);
+router.delete('/operations/delete/:id', auth, operationsController.deleteOperations);
+router.get('/operations/get/:id', auth, operationsController.getOperationById);
+router.get('/operations/prices/:userId', auth, operationsController.getPrices);
 
 
 module.exports = router;
